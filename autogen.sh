e23328127dee2321942fc40a7852ccce8783ffd2
Jérôme Guelfucci <jeromeg@xfce.org>
2011-04-23 09:10:55 +0200

    Update autogen.sh.

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 #
-# $Id$
+# Copyright (c) 2002-20011 The Xfce development team. All rights reserved.
 #
-# Copyright (c) 2002-2007
-#         The Xfce development team. All rights reserved.
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Written for Xfce by Benedikt Meurer <benny@xfce.org>.
 #
 
 (type xdt-autogen) >/dev/null 2>&1 || {
@@ -16,20 +29,5 @@ EOF
   exit 1
 }
 
-# verify that po/LINGUAS is present
-(test -f po/LINGUAS) >/dev/null 2>&1 || {
-  cat >&2 <<EOF
-autogen.sh: The file po/LINGUAS could not be found. Please check your snapshot
-            or try to checkout again.
-EOF
-  exit 1
-}
-
-# substitute revision and linguas
-linguas=`sed -e '/^#/d' po/LINGUAS`
-revision=`LC_ALL=C svn info $0 | awk '/^Revision: / {printf "%04d\n", $2}'`
-sed -e "s/@LINGUAS@/${linguas}/g" \
-    -e "s/@REVISION@/${revision}/g" \
-    < "configure.in.in" > "configure.in"
-
+XDT_AUTOGEN_REQUIRED_VERSION="4.7.3" \
 exec xdt-autogen $@
