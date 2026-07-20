a8f15c5817cc9b0f9151fdb1179c0f98b9b0352a
Gaël Bonithon <gael@xfce.org>
2026-07-20 12:44:57 +0200

    Do not compare gboolean to TRUE or FALSE
    See https://docs.gtk.org/glib/types.html#gboolean

--- panel-plugin/sample-dialogs.c
+++ panel-plugin/sample-dialogs.c
@@ -50,7 +50,7 @@ sample_configure_response (GtkWidget    *dialog,
 #else
       result = g_spawn_command_line_async ("exo-open --launch WebBrowser " PLUGIN_WEBSITE, NULL);
 #endif
-      if (G_UNLIKELY (result == FALSE))
+      if (G_UNLIKELY (!result))
         g_warning (_("Unable to open the following url: %s"), PLUGIN_WEBSITE);
     }
   else
